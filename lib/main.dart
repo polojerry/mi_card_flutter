c29aec8d36689caf5799feb82d2e29f39c668aa6
polojerry <polojerry4@gmail.com>
2020-05-20 09:30:06 +0300

    Added Icons

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MiCard());
@@ -38,6 +39,52 @@ class MiCard extends StatelessWidget {
                     color: Colors.teal.shade100),
                 textAlign: TextAlign.center,
               ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.call,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+254 790 689 212',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal,
+                          fontFamily: 'SourceSansPro'),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'polojerry4@gmail.com',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal,
+                          fontFamily: 'SourceSansPro'),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
