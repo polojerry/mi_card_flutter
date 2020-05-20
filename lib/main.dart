488d4c18213a1acfd9c3598048fa4dc89e71e55b
polojerry <polojerry4@gmail.com>
2020-05-20 08:28:00 +0300

    Added Circle Avatar, Customized Text

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,18 @@ class MiCard extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 100,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/profile.jpeg"),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
-              Container(
-                width: 100,
-                color: Colors.blue,
+              Text(
+                "Jeremiah Polo",
+                style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
             ],
           ),
