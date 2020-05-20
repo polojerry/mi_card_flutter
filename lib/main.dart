232671a47f543f6cfbc4b7339e787991a30d3eca
polojerry <polojerry4@gmail.com>
2020-05-20 07:03:59 +0300

    Used a SafeArea Widget, Container Widget. Added Padding and Margin to a container

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,13 @@ class MiCard extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.fromLTRB(8.0, 36.0, 8, 8),
+            padding: EdgeInsets.all(20.0),
+            child: Text("Hello World"),
+          ),
+        ),
       ),
     );
   }
